1910601192dbadbc09f7a8bfe7d360cf226e8dfa
proanimer <1322767102@qq.com>
2023-12-26 00:01:27 +0800

    add chatbot

--- app.py
+++ app.py
@@ -1,15 +1,72 @@
 ***REMOVED***
-
-
-def greet(name, intensity,test):
+***REMOVED***
+***REMOVED***
 ***REMOVED***
 
 
-demo = gr.Interface(
-    fn=greet,
-    inputs=["text", gr.components.Slider()],
-    outputs=["text"],
-)
+***REMOVED***
+***REMOVED***
+
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+            "Authorization": "Bearer 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",
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+***REMOVED***
+            "stream": True,
+***REMOVED***
+***REMOVED***
+***REMOVED***
+    print(response,temperature)
+***REMOVED***ponse.json()["content"]
+
+
+***REMOVED***
+***REMOVED***
+***REMOVED***
+    chat_demo = gr.ChatInterface(req_bot, additional_inputs=[temperature],examples=["Hello", "Am I cool?", "Are tomatoes vegetables?"],
+    cache_examples=True).queue()
+
+***REMOVED***
 
 ***REMOVED***
     demo.launch()
